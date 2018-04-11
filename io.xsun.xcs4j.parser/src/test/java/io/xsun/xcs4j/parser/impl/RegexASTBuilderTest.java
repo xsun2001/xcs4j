252131f0e403dfc821e1379554c413536fb3ca06
xsun2001 <1563770452@qq.com>
2018-04-11 23:25:32 +0800

    add more tests

--- io.xsun.xcs4j.parser/src/test/java/io/xsun/xcs4j/parser/impl/RegexASTBuilderTest.java
+++ io.xsun.xcs4j.parser/src/test/java/io/xsun/xcs4j/parser/impl/RegexASTBuilderTest.java
@@ -2,16 +2,35 @@ package io.xsun.xcs4j.parser.impl;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static io.xsun.xcs4j.parser.impl.RegexASTNode.NodeType.*;
+import static org.junit.Assert.assertEquals;
 
 public class RegexASTBuilderTest {
 
+    public void testBuildAST(String regex, RegexASTNode answer) {
+        assertEquals(new RegexASTBuilder(regex).buildAST(), answer);
+    }
+
     @Test
     public void testBuildAST1() {
-        var regex = "a";
-        var builder = new RegexASTBuilder(regex);
-        var ast = builder.buildAST();
-        var ans = new RegexASTNode('a');
-        assertEquals(ans, ast);
+        testBuildAST("a", new RegexASTNode('a'));
+    }
+
+    @Test
+    public void testBuildAST2() {
+        var answer = new RegexASTNode(CAT, new RegexASTNode('a'), new RegexASTNode('b'));
+        testBuildAST("ab", answer);
+    }
+
+    @Test
+    public void testBuildAST3() {
+        var answer = new RegexASTNode(STAR, new RegexASTNode('a'), null);
+        testBuildAST("a*", answer);
+    }
+
+    @Test
+    public void testBuildAST4() {
+        var answer = new RegexASTNode(OR, new RegexASTNode('a'), new RegexASTNode('b'));
+        testBuildAST("a|b", answer);
     }
 }
