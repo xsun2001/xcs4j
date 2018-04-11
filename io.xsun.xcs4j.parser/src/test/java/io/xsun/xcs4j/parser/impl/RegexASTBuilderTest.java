aad82102fd5a0a143c84c5f1ecb35d516115452e
xsun2001 <1563770452@qq.com>
2018-04-11 23:46:20 +0800

    add two more complicated tests

--- io.xsun.xcs4j.parser/src/test/java/io/xsun/xcs4j/parser/impl/RegexASTBuilderTest.java
+++ io.xsun.xcs4j.parser/src/test/java/io/xsun/xcs4j/parser/impl/RegexASTBuilderTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertEquals;
 public class RegexASTBuilderTest {
 
     public void testBuildAST(String regex, RegexASTNode answer) {
-        assertEquals(new RegexASTBuilder(regex).buildAST(), answer);
+        assertEquals(answer, new RegexASTBuilder(regex).buildAST());
     }
 
     @Test
@@ -32,5 +32,35 @@ public class RegexASTBuilderTest {
     public void testBuildAST4() {
         var answer = new RegexASTNode(OR, new RegexASTNode('a'), new RegexASTNode('b'));
         testBuildAST("a|b", answer);
+    }
+
+    @Test
+    public void testBuildAST5() {
+        var node1 = new RegexASTNode(CAT, new RegexASTNode('a'), new RegexASTNode('b'));
+        var node2 = new RegexASTNode(CAT, new RegexASTNode('b'), new RegexASTNode('a'));
+        var node3 = new RegexASTNode(STAR, new RegexASTNode(OR, node1, node2), null);
+        var answer = new RegexASTNode(CAT,
+                new RegexASTNode(CAT,
+                        node3,
+                        new RegexASTNode('a')),
+                new RegexASTNode('b')
+        );
+
+        testBuildAST("(ab|ba)*ab", answer);
+    }
+
+    @Test
+    public void testBuildAST6(){
+        var node1 = new RegexASTNode(STAR, new RegexASTNode(OR, new RegexASTNode('a'), new RegexASTNode('b')), null);
+        var answer = new RegexASTNode(CAT,
+                new RegexASTNode(CAT,
+                        new RegexASTNode(CAT,
+                                new RegexASTNode(CAT, node1, new RegexASTNode('a')),
+                                new RegexASTNode('b')),
+                        new RegexASTNode('b')),
+                node1
+        );
+
+        testBuildAST("(a|b)*abb(a|b)*", answer);
     }
 }
